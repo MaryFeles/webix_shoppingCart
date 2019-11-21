105eecfc12ef5926e80c3496ac197788ebd4d844
PavelR <repening.pavel@gmail.com>
2019-11-21 16:29:20 +0400

    added a function to calculate the total cost of products

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ webix.ready(function() {
     let myGUI = webix.ui({
         rows:[
             {rows:[{template:"Магазин"}], height: 80},
-            {cols:[storageTable, bascketTable]},
-            {template: "Итого:", height: 80}
-        ], 
+            {cols:[storageTable, bascketTable], height: 300},
+            { view:"text", id:"sum", value:"0", label:"Итого: ", width: 200}
+        ],
     });
     $$('storageTable').parse(storage);
     $$('bascketTable').parse(bascket);
-
 });
 
 let storageTable = {
@@ -28,6 +27,7 @@ let storageTable = {
     },
 };
 
+
 let bascketTable = {
     view: "datatable",
     id: "bascketTable",
@@ -40,54 +40,67 @@ let bascketTable = {
     ],
     on:{
         "onItemClick": function(id, e, trg) {
-        	//webix.message("Click on row: " + id.row+", column: " + id.column)
         	removeFromBascket();
         }
     },
 };
 
+
+//функция добавления товара в корзину
 function addToBascket(){
 	productQuantity = $$('storageTable').getSelectedItem().quantity;
-	if (productQuantity == 0){
+	if (productQuantity == 0){ // если на складе нет товара
 		alert('Failed');
-	} else {		
-		$$('storageTable').getSelectedItem().quantity--;
+	} else {		           // если на складе есть товар
+		$$('storageTable').getSelectedItem().quantity--; // уменьшаем на складе количество товара на единицу
 		$$('storageTable').refresh();
 
-		let selected_id = $$("storageTable").getItem($$("storageTable").getSelectedId(true)).id;
-		let copyRow = webix.copy( $$('storageTable').getSelectedItem());
+		let selected_id = $$("storageTable").getItem($$("storageTable").getSelectedId(true)).id; //получаем id выбранного товара
+		let copyRow = webix.copy( $$('storageTable').getSelectedItem()); // копируем выбранный товар
 
-		if($$('bascketTable').exists(selected_id)){
+		if($$('bascketTable').exists(selected_id)){ //если в корзине уже есть товар с данным id 
 			console.log("exists");
 
-			$$('bascketTable').getItem(selected_id).quantity++;
-			$$('bascketTable').getItem(selected_id).price+=$$('storageTable').getItem(selected_id).price;
+			$$('bascketTable').getItem(selected_id).quantity++; // в корзине увеличиваем количество товара на единицу
+			$$('bascketTable').getItem(selected_id).price+=$$('storageTable').getItem(selected_id).price; //увеличиваем стоимость на цену товара
 			$$('bascketTable').refresh();
-		} else{
-			$$('bascketTable').add(copyRow);
-			$$('bascketTable').getItem(selected_id).quantity = 1;
+		} else{ // если в корзине нет товара с таким id
+			$$('bascketTable').add(copyRow); // добавляем в корзину
+			$$('bascketTable').getItem(selected_id).quantity = 1; 
 			$$('bascketTable').refresh();
 		}    
 	}
+	totalPrice();
 }
 
-function removeFromBascket(){
-	let selected_id = $$('bascketTable').getItem($$('bascketTable').getSelectedId(true)).id;
-	if( $$('bascketTable').getSelectedItem().quantity == 1 ){
-		$$('storageTable').getItem(selected_id).quantity++;
-		$$('storageTable').refresh();
-		$$('bascketTable').remove(selected_id);
+//функция подсчета итоговой стоимости товаров в корзине
+function totalPrice(){
+	let sum = 0;
+	$$('bascketTable').eachRow(function(row){
+		sum += $$('bascketTable').getItem(row).price;
+	});	
+	$$("sum").setValue(sum);
 
+
+}
+
+//функция удаления товара из корзины
+function removeFromBascket(){ 
+	let selected_id = $$('bascketTable').getItem($$('bascketTable').getSelectedId(true)).id;  //получаем id выбранного товара
+	if( $$('bascketTable').getSelectedItem().quantity == 1 ){ // если в корзине у выбранного товара количество равно 1
+		$$('storageTable').getItem(selected_id).quantity++; // на складе увеличиваем количество на единицу
+		$$('storageTable').refresh();
+		$$('bascketTable').remove(selected_id); // удаляем строку из корзины
 
 	} else {
-		$$('storageTable').getItem(selected_id).quantity++;
+		$$('storageTable').getItem(selected_id).quantity++; // на складе увеличиваем количество товара на единицу
 		$$('storageTable').refresh();
 
-		$$('bascketTable').getSelectedItem().quantity--;
-		$$('bascketTable').getItem(selected_id).price-=$$('storageTable').getItem(selected_id).price;
-		$$('bascketTable').refresh();		
-	}
-	
+		$$('bascketTable').getSelectedItem().quantity--; // в корзине уменьшаем количество на единицу
+		$$('bascketTable').getItem(selected_id).price-=$$('storageTable').getItem(selected_id).price; //уменьшаем стоимость 
+		$$('bascketTable').refresh();
+	}	
+	totalPrice();
 }
 
 
